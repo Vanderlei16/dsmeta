e17fab331c3eda3a59a9ba0b8e3002af7e6dd463
Vanderlei Oliveira <edbvander@gmail.com>
2022-07-12 21:29:51 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
+import NotificationButon from "./components/NotificationButton"
+
 function App() {
- 
-return(
 
-<h1>Olá mundo !</h1>
+  return (
+    <>
+      <h1>Olá mundo !</h1>
+      <NotificationButon />
+      <NotificationButon />
+    </>
 
-)
-
+  )
 }
-
 export default App
